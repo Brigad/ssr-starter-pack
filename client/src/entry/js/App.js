605adbe6945b907d86cb38649cd853996519a5ea
Jose Luis Irrazabal <joseirrazabal@gmail.com>
2018-05-31 17:49:12 -0300

    webpack4

--- client/src/entry/js/App.js
+++ client/src/entry/js/App.js
@@ -1,18 +1,19 @@
-import '../css/App.scss';
+import "../css/App.scss";
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import Head from './components/Head';
-import ClientRouting from './components/ClientRouting';
-import ServerRouting from './components/ServerRouting';
+import Head from "./components/Head";
+import ClientRouting from "./components/ClientRouting";
+import ServerRouting from "./components/ServerRouting";
 
 const App = ({ type, url, context }) => {
-  const Routing = type === 'client' ? (
-    <ClientRouting />
-  ) : (
-    <ServerRouting url={url} context={context} />
-  );
+  const Routing =
+    type === "client" ? (
+      <ClientRouting />
+    ) : (
+      <ServerRouting url={url} context={context} />
+    );
 
   return (
     <div>
@@ -23,14 +24,14 @@ const App = ({ type, url, context }) => {
 };
 
 App.propTypes = {
-  type: PropTypes.oneOf(['client', 'server']).isRequired,
+  type: PropTypes.oneOf(["client", "server"]).isRequired,
   url: PropTypes.string,
-  context: PropTypes.object,
+  context: PropTypes.object
 };
 
 App.defaultProps = {
-  url: '',
-  context: undefined,
+  url: "",
+  context: undefined
 };
 
 export default App;
--- client/src/entry/js/server.js
+++ client/src/entry/js/server.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { Helmet } from "react-helmet";
 
-import App from './App';
+import App from "./App";
 
-import { initializeServerSideHeaders } from 'src/utils/EnvUtils';
-import { isMobileBrowser } from 'src/utils/MobileUtils';
+import { initializeServerSideHeaders } from "src/utils/EnvUtils";
+import { isMobileBrowser } from "src/utils/MobileUtils";
 
-import { getPaceLoadingBarStyle, getPaceLoadingBarScript } from 'src/utils/InlineScriptsUtils';
+import {
+  getPaceLoadingBarStyle,
+  getPaceLoadingBarScript
+} from "src/utils/InlineScriptsUtils";
 
 const render = manifests => (req, res) => {
   initializeServerSideHeaders(req.headers);
 
   const context = {
-    splitPoints: [],
+    splitPoints: []
   };
 
   const markup = renderToString(
-    <App type="server" url={req.url} context={context} />,
+    <App type="server" url={req.url} context={context} />
   );
 
   if (context.url) {
@@ -26,8 +29,8 @@ const render = manifests => (req, res) => {
 
   const helmet = Helmet.renderStatic();
 
-  const LoadingBarStyle = !isMobileBrowser() ? getPaceLoadingBarStyle() : '';
-  const LoadingBarScript = !isMobileBrowser() ? getPaceLoadingBarScript() : '';
+  const LoadingBarStyle = !isMobileBrowser() ? getPaceLoadingBarStyle() : "";
+  const LoadingBarScript = !isMobileBrowser() ? getPaceLoadingBarScript() : "";
 
   const SplitPointsScript = `
     <script>
@@ -35,9 +38,12 @@ const render = manifests => (req, res) => {
       window.serverSideHeaders = ${JSON.stringify(req.headers)};
     </script>
   `;
-  const ChunkManifestScript = manifests.client ? `
-    <script src="${manifests.client['manifest.js']}"></script>
-  ` : '';
+  const ChunkManifestScript =
+    manifests.client && manifests.client["manifest.js"]
+      ? `
+    <script src="${manifests.client["manifest.js"]}"></script>
+  `
+      : "";
 
   return res.send(`
     <!doctype html>
@@ -48,18 +54,30 @@ const render = manifests => (req, res) => {
         ${helmet.link.toString()}
         ${helmet.script.toString()}
         ${helmet.noscript.toString()}
-        
-        <link rel="stylesheet" href="${!manifests.server ? '/dist/server/main.css' : manifests.server['main.css']}" />
+
+        <link rel="stylesheet" href="${
+          !manifests.server
+            ? "/dist/server/main.css"
+            : manifests.server["main.css"]
+        }" />
         ${LoadingBarStyle}
       </head>
       <body>
         <div id="content">${markup}</div>
-        
+
         ${LoadingBarScript}
         ${SplitPointsScript}
         ${ChunkManifestScript}
-        <script src="${!manifests.client ? '/dist/client/vendors.js' : manifests.client['vendors.js']}"></script>
-        <script src="${!manifests.client ? '/dist/client/main.js' : manifests.client['main.js']}"></script>
+        <script src="${
+          !manifests.client
+            ? "/dist/client/chunks/vendors.js"
+            : manifests.client["vendors.js"]
+        }"></script>
+        <script src="${
+          !manifests.client
+            ? "/dist/client/main.js"
+            : manifests.client["main.js"]
+        }"></script>
       </body>
     </html>
   `);
